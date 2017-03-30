70c4766c56887eb301d18d8dfac5845060b0a34b
Tanner Owens <tlowens@wisc.edu>
2017-03-29 19:20:49 -0500

    main activity updated and added schedule activity

---
+++ app/src/main/java/com/tannerowens/a407_roommate_app/ScheduleActivity.java
@@ -0,0 +1,24 @@
+package com.tannerowens.a407_roommate_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Tanner on 3/29/2017.
+ */
+
+public class ScheduleActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_schedule);
+
+        method();
+    }
+
+    public void method(){
+
+    }
+}
