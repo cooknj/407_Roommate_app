91b555a129cfec4ac85933820053d38881b767a4
Tanner Owens <tlowens@wisc.edu>
2017-03-31 08:28:08 -0500

    edits to schedule and fixed intent calling errors

--- app/src/main/java/com/tannerowens/a407_roommate_app/ScheduleActivity.java
+++ app/src/main/java/com/tannerowens/a407_roommate_app/ScheduleActivity.java
@@ -1,11 +1,10 @@
 package com.tannerowens.a407_roommate_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-import static com.tannerowens.a407_roommate_app.R.id.homeButton;
 
 /**
  * Created by Tanner on 3/29/2017.
@@ -20,14 +19,27 @@ public class ScheduleActivity extends AppCompatActivity{
         setContentView(R.layout.my_schedule);
 
         configureHomeButton();
+        configureAddActivityButton();
     }
 
     private void configureHomeButton() {
-        Button button = (Button) findViewById(homeButton);
+        Button button = (Button) findViewById(R.id.homeButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+    }
+
+    private void configureAddActivityButton(){
+        Button button = (Button) findViewById(R.id.addActivityButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ScheduleActivity.this, ScheduleEntryActivity.class);
+                //intent.putExtra("table1", 1);
+                startActivityForResult(intent, 1);//should receive text we wish to enter into table
             }
         });
     }
