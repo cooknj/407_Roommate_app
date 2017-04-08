6bb2af0c65d1eda0548f7c6c26084e7f7aa3c700
Tanner Owens <tlowens@wisc.edu>
2017-04-08 15:37:10 -0500

    Schedule updated and stored in DB correctly

--- app/src/main/java/com/tannerowens/a407_roommate_app/MainActivity.java
+++ app/src/main/java/com/tannerowens/a407_roommate_app/MainActivity.java
@@ -141,6 +141,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ScheduleActivity.class);
+                intent.putExtra("user", user);
                 startActivity(intent);
             }
         });
