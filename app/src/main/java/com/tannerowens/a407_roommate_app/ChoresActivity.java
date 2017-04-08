5986c4e97d4be56343143f722194763c2056c51a
cooknj <ncook3@wisc.edu>
2017-04-08 12:40:22 -0500

    Plethora of changes for proper functionality
    Biggest change: GlobalChoreList changed from own object to wrapper class
    for HashMap of chores. Removed all methods and only have a singleton
    constructor and get() method.
    
    Add Chore is functional
    
    UncompletedChoresActivity is functional though not aesthetic by any
    means.
    
    Added content to MyChores - untested. Need to get User obj and name from
    user before functional & testable.
    
    Various minor changes to .xmls for functionality.
    
    Next tasks:
    1. Get current user from firebase.
    2. Upload and store choreMap to firebase for persistency.

--- app/src/main/java/com/tannerowens/a407_roommate_app/ChoresActivity.java
+++ app/src/main/java/com/tannerowens/a407_roommate_app/ChoresActivity.java
@@ -6,9 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import static com.tannerowens.a407_roommate_app.R.id.addChores;
-import static com.tannerowens.a407_roommate_app.R.id.completedChores;
-import static com.tannerowens.a407_roommate_app.R.id.myChores;
 
 /**
  * Created by Nick on 3/30/2017.
@@ -20,39 +17,62 @@ public class ChoresActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chores_main);
 
+        configureBackButton();
+        configureHomeButton();
         configureMyChoresButton();
         configureCompletedChoresButton();
         configureAddChoresButton();
     }
 
-    private void configureMyChoresButton() {
-        Button button = (Button) findViewById(myChores);
+    private void configureBackButton() {
+        Button button = (Button) findViewById(R.id.backButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), MyChoresActivity.class);
+                finish();
+            }
+        });
+    }
+
+    private void configureHomeButton() {
+        Button button = (Button) findViewById(R.id.homeButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChoresActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void configureMyChoresButton() {
+        Button button = (Button) findViewById(R.id.myChores);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChoresActivity.this, MyChoresActivity.class);
                 startActivity(intent);
             }
         });
     }
 
     private void configureCompletedChoresButton() {
-        Button button = (Button) findViewById(completedChores);
+        Button button = (Button) findViewById(R.id.uncompletedChores);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), CompletedChoresActivity.class);
+                Intent intent = new Intent(ChoresActivity.this, UncompletedChoresActivity.class);
                 startActivity(intent);
             }
         });
     }
 
     private void configureAddChoresButton() {
-        Button button = (Button) findViewById(addChores);
+        Button button = (Button) findViewById(R.id.addChores);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), AddChoresActivity.class);
+                Intent intent = new Intent(ChoresActivity.this, AddChoresActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/tannerowens/a407_roommate_app/MainActivity.java
+++ app/src/main/java/com/tannerowens/a407_roommate_app/MainActivity.java
@@ -2,6 +2,7 @@ package com.tannerowens.a407_roommate_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     private String username;
     private String name;
     private User user;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
