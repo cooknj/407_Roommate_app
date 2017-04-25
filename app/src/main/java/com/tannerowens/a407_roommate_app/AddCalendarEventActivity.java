89ee0c1627f5fb4cf4e6fc273d69d713c82e3eab
sweideman <sweideman@wisc.edu>
2017-04-25 14:37:15 -0500

    Update AddCalendarEventActivity.java

--- app/src/main/java/com/tannerowens/a407_roommate_app/AddCalendarEventActivity.java
+++ app/src/main/java/com/tannerowens/a407_roommate_app/AddCalendarEventActivity.java
@@ -1,12 +1,12 @@
 package com.tannerowens.a407_roommate_app;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -22,6 +22,8 @@ public class AddCalendarEventActivity extends AppCompatActivity{
     private int month;
     private int year;
     private User user;
+    private House group;
+    private RadioButton personal;
     private DatabaseReference mDatabase;
 
     @Override
@@ -35,7 +37,9 @@ public class AddCalendarEventActivity extends AppCompatActivity{
         year = (Integer) getIntent().getSerializableExtra("year");
         user = (User) getIntent().getSerializableExtra("user");
         mDatabase = FirebaseDatabase.getInstance().getReferenceFromUrl("https://roommateapp-a6d3a.firebaseio.com/");
+        personal = (RadioButton) findViewById(R.id.radioMe);
 
+        getHouse();
         configureDateText();
         configureBackButton();
         configureAddButton();
@@ -73,22 +77,46 @@ public class AddCalendarEventActivity extends AppCompatActivity{
                 EditText end_txt = (EditText) findViewById(R.id.EventTimeEnd);
                 String endTime = end_txt.getText().toString();
 
-               /* Intent intent = new Intent();
-                intent.putExtra("name", name);
-                intent.putExtra("location", location);
-                intent.putExtra("startTime", startTime);
-                intent.putExtra("endTime", endTime);
-                intent.putExtra("month", month);
-                intent.putExtra("day", day);
-                intent.putExtra("year", year);
-                setResult(1, intent);*/
                 CalendarEvent event = new CalendarEvent(name, location, startTime, endTime, month, day, year);
-                user.addCalendarEvent(event);
-                mDatabase.child("users").child(user.getUsername()).setValue(user);
+                if (!personal.isChecked()) {
+                    if (user.getHouse() != null) {
+                        for (int i = 0; i < group.getUsers().size(); i++) {
+                            User currUser = group.getUsers().get(i);
+                            currUser.addCalendarEvent(event);
+                            mDatabase.child("users").child(currUser.getUsername()).setValue(currUser);
+                        }
+                    }
+                }
+                else {
+                    user.addCalendarEvent(event);
+                    mDatabase.child("users").child(user.getUsername()).setValue(user);
+                }
                 finish();
 
             }
         });
     }
 
+    private void getHouse() {
+        if (user.getHouse() != null) {
+            DatabaseReference mRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://roommateapp-a6d3a.firebaseio.com/house");
+            mRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot house : dataSnapshot.getChildren()) {
+                        if (user.getHouse().equals(house.getKey())) {
+                            House h = house.getValue(House.class);
+                            group = h;
+                        }
+                    }
+                }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
 }
+
